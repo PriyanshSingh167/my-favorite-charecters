13a6aafa319ccd70524e342708dff45509c0a3bb
priyanshsingh <priyansh@singh.com>
2022-03-08 15:24:13 +0530

    First version

---
+++ src/components/App.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import Card from "./Card";
+import Avatar from "./Avatar";
+import contacts from "../contacts";
+
+function App() {
+  return (
+    <div>
+      <h1 className="heading">My Contacts</h1>
+      <Avatar img="https://lh3.googleusercontent.com/a-/AOh14GhxKJoRqX-Hr34ANDtB40MKHq1w9FBotrNd-7wUwQ=s432-p-rw-no" />
+
+      <Card
+        name={contacts[0].name}
+        img={contacts[0].imgURL}
+        tel={contacts[0].phone}
+        email={contacts[0].email}
+      />
+      <Card
+        name={contacts[1].name}
+        img={contacts[1].imgURL}
+        tel={contacts[1].phone}
+        email={contacts[1].email}
+      />
+      <Card
+        name={contacts[2].name}
+        img={contacts[2].imgURL}
+        tel={contacts[2].phone}
+        email={contacts[2].email}
+      />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Details.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+
+function Deatils(props) {
+  return <p className="info">{props.detailInfo}</p>;
+}
+export default Deatils;
